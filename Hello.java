19259ed75d7b67a184352078ce5eb413c6d54fab
gkraud4135 <77388722+gkraud4135@users.noreply.github.com>
2021-03-17 13:05:24 +0900

    Add files via upload

--- Hello.java
+++ Hello.java
@@ -11,10 +11,31 @@ package chapter1;
 	변수선언 : 메모리 공간의 할당
 */
 
+//컨트롤 쉬프트 f ===== 정리
+//print 줄내림x println 줄내림O
+
 public class Hello {
 
 	public static void main(String[] args) {
-//		변수생성
+		ex1();
+		// 논리 연산자 && || !
+		int num1, num2;
+		num1 =11;
+		num2 =22;
+		boolean result;
+		//변수 num1에 저장된 값이 1과 100사이의 수인가?
+		result = (1<num1) && (num1<100);
+		System.out.println("1초과 100 미만 수인가?" + result);
+		//변수 num2에 저장된 값이 2또는 3의 배수인가?
+		result = (num2%2==0) || (num2%3==0); // (num2%2) == 0 || (num2%3) == 0;
+		System.out.println("2또는 3의 배수인가?"+ result); // true
+		// not연산자
+		System.out.println(!result); //false
+
+	}
+
+	public static void ex1() { // ex1 메서드생성
+		// 변수생성
 		int num = 9;
 		System.out.println(num);
 
@@ -50,6 +71,55 @@ public class Hello {
 
 		int iNum = 90; // 형변환 숫자 > 글 유니코드 90의 글자는 Z
 		System.out.println((char) iNum);
+
+		char ch2 = '헐';
+		System.out.println((int) ch2);
+
+		char ch3 = '\uAFB9'; // 유니코드에 따른 그 값 출력
+		System.out.println(ch3);
+
+		boolean yesNo = true; // 참 거짓 트루
+		System.out.println(yesNo);
+		if (yesNo)
+			System.out.println("yesNo의값 true 입니다");
+		else
+			System.out.println("yesNo의값 false 입니다");
+
+		int a, b, c; // 거짓이기에 flase
+		a = 10;
+		b = 20;
+		c = 30;
+		System.out.println(a > b);
+
+		// 문자는 객체자료형으로 (대문자) String이라는 클래스가 존재
+		String str = "가나다라";
+		String str1 = "마바사";
+		String str2 = "아자차카";
+		System.out.println(str + str1 + str2);
+
+		final double PI = 3.14d; // 변수가 아닌 고정된 값이기에 final을 붙여서 저장 -- 초기화된 상수 변경불가
+
+	}
+
+	public static void ex2() {
+		int a, b;
+		a = 5;
+		b = 7;
+		System.out.println(a + b); // 12
+		System.out.println(a - b); // -2
+		System.out.println(a * b); // 35
+		System.out.println(b / a); // 1
+		System.out.println(b % a); // 2
+		// 관계 연산자 크기
+		System.out.println(a > b); // false
+		System.out.println(a < b); // true
+		System.out.println(a >= b); // flase
+		System.out.println(a <= b); // true
+		System.out.println(a == b); // flase 같은가
+		System.out.println(a != b); // true 다른가
+		// 복합대입 연산자
+		System.out.println(a += 1); // 6
+		System.out.println(a += 2); // 8 위에서 아래로 a + 1 + 2 = 8
 
 	}
 
